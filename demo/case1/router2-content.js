e937804e7ed3952d044cff127e3460e2be660c12
Andres Rios <rianby64@gmail.com>
2016-07-18 19:33:13 +0300

    Add more complexity to the simple example

--- demo/case1/router2-content.js
+++ demo/case1/router2-content.js
@@ -1,6 +1,26 @@
 ;(_ => {
   'use strict';
 
+  function matchHash() {
+    var containers = document.querySelectorAll('router2-content');
+    for (var i = 0; i < containers.length; i++) {
+      containers[i].hidden = true;
+    }
+
+    var hash = window.location.hash.slice(1);
+    // nothing to unhide...
+    if (!hash) {
+      return;
+    }
+
+    var matched = document.querySelector(`router2-content[hash="${hash}"]`);
+    if (matched) {
+      matched.hidden = false;
+    } else {
+      throw new Error(`hash "${hash}" does not match any content`);
+    }
+  }
+
   class Router2Content extends HTMLElement {
     createdCallback() {
       this.hidden = true; // by default
@@ -8,16 +28,11 @@
   }
 
   window.addEventListener('hashchange', (e) => {
-    var containers = document.querySelectorAll('router2-content');
-    for (var i = 0; i < containers.length; i++) {
-      containers[i].hidden = true;
-    }
-
-    var hash = window.location.hash.slice(1);
-    var matched = document.querySelector(`router2-content[hash="${hash}"]`);
-    if (matched) {
-      matched.hidden = false;
-    }
+    matchHash();
   });
+  window.addEventListener('load', (e) => {
+    matchHash();
+  });
+
   document.registerElement('router2-content', Router2Content);
 })();
