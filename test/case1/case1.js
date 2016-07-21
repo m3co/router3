eea7393263c329360f89c3b0292f673f855d7f14
Andres Rios <rianby64@gmail.com>
2016-07-21 19:01:03 +0300

    Rewrite test case1.js following the example

--- test/case1/case1.js
+++ test/case1/case1.js
@@ -37,7 +37,7 @@
   var async1 = async_test('Test content[hash="a-hash-template"]');
   var async2 = async_test('Test content[hash="another-hash-template"]');
 
-  async1.step(_ => {
+  async1.next = async1.step_func(_ => {
     var hash = "a-hash-template";
     var content = document.querySelector(`${tagContent}[hash="${hash}"]`);
     assert_true(content.hidden);
@@ -53,34 +53,40 @@
       hashAsync.done();
       async1.done();
 
+      async2.next();
 
-      async2.step(_ => {
-        var hash = "another-hash-template";
-        var content = document.querySelector(`${tagContent}[hash="${hash}"]`);
-        assert_true(content.hidden);
-
-        var hashAsync = async_test(`If change hash to "${hash}" then show its content`);
-
-        var check_hash = hashAsync.step_func((e) => {
-          assert_false(content.hidden);
-
-          // clean the test
-          window.removeEventListener('hashchange', check_hash);
-          window.location.hash = '';
-
-          hashAsync.done();
-          async2.done();
-          document.body.removeChild(div);
-        });
-
-        window.addEventListener('hashchange', check_hash);
-        window.location.hash = hash;
-
-      });
     });
 
     window.addEventListener('hashchange', check_hash);
     window.location.hash = hash;
+  });
+
+  async2.next = async2.step_func(_ => {
+    var hash = "another-hash-template";
+    var content = document.querySelector(`${tagContent}[hash="${hash}"]`);
+    assert_true(content.hidden);
+
+    var hashAsync = async_test(`If change hash to "${hash}" then show its content`);
+
+    var check_hash = hashAsync.step_func((e) => {
+      assert_false(content.hidden);
+
+      // clean the test
+      window.removeEventListener('hashchange', check_hash);
+      window.location.hash = '';
+
+      hashAsync.done();
+      async2.done();
+      document.body.removeChild(div);
+    });
+
+    window.addEventListener('hashchange', check_hash);
+    window.location.hash = hash;
+
+  });
+
+  async1.step(_ => {
+    async1.next();
   });
 
 })();
