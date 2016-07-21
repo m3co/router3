27a19e21ac02fb656a4eadc0707948f983e34fee
Andres Rios <rianby64@gmail.com>
2016-07-21 19:22:55 +0300

    Test if click over the a href=...

--- test/case1/case1.js
+++ test/case1/case1.js
@@ -34,8 +34,10 @@
     assert_true(window.location.hash === '' || window.location.hash === '');
   }, 'The location starts with no hash');
 
-  var async1 = async_test('Test content[hash="a-hash-template"]');
-  var async2 = async_test('Test content[hash="another-hash-template"]');
+  var async1 = async_test('hash changed for content[hash="a-hash-template"]');
+  var async2 = async_test('hash changed for content[hash="another-hash-template"]');
+  var async3 = async_test('click over href="#a-hash-template"');
+  var async4 = async_test('click over href="#another-hash-tempate"');
 
   async1.next = async1.step_func(_ => {
     var hash = "a-hash-template";
@@ -52,9 +54,7 @@
 
       hashAsync.done();
       async1.done();
-
       async2.next();
-
     });
 
     window.addEventListener('hashchange', check_hash);
@@ -77,12 +77,57 @@
 
       hashAsync.done();
       async2.done();
-      document.body.removeChild(div);
+      async3.next();
     });
 
     window.addEventListener('hashchange', check_hash);
     window.location.hash = hash;
 
+  });
+
+  async3.next = async3.step_func(_ => {
+    var hash = "a-hash-template";
+    var content = document.querySelector(`${tagContent}[hash="${hash}"]`);
+    assert_true(content.hidden);
+    var hashAsync = async_test(`If click over a[href="${hash}"] then show its content`);
+
+    var check_hash = hashAsync.step_func((e) => {
+      assert_false(content.hidden);
+
+      // clean the test
+      window.removeEventListener('hashchange', check_hash);
+      window.location.hash = '';
+
+      hashAsync.done();
+      async3.done();
+      async4.next();
+    });
+
+    window.addEventListener('hashchange', check_hash);
+    document.querySelector(`a[href="#${hash}"]`).dispatchEvent(new Event('click'));
+  });
+
+  async4.next = async4.step_func(_ => {
+    var hash = "another-hash-template";
+    var content = document.querySelector(`${tagContent}[hash="${hash}"]`);
+    assert_true(content.hidden);
+
+    var hashAsync = async_test(`If click over a[href="${hash}"] then show its content`);
+
+    var check_hash = hashAsync.step_func((e) => {
+      assert_false(content.hidden);
+
+      // clean the test
+      window.removeEventListener('hashchange', check_hash);
+      window.location.hash = '';
+
+      hashAsync.done();
+      async4.done();
+      document.body.removeChild(div);
+    });
+
+    window.addEventListener('hashchange', check_hash);
+    document.querySelector(`a[href="#${hash}"]`).dispatchEvent(new Event('click'));
   });
 
   async1.step(_ => {
