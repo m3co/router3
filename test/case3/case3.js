2cf1cdda49c21c3d258c11b95f3b1870854a8f3f
Andres Rios <rianby64@gmail.com>
2016-07-25 10:25:51 +0300

    Give an id to the parent content at test 3

--- test/case3/case3.js
+++ test/case3/case3.js
@@ -5,7 +5,7 @@
 
   var div = document.createElement('div');
   div.innerHTML = `
-  <${tagContent} hash="case3-location1">
+  <${tagContent} id="case3-test1-p" hash="case3-location1">
     <${tagContent} id="case3-test1" hash="case3-location2">
       Location 2
     </${tagContent}>
@@ -15,17 +15,28 @@
   <${tagContent} id="case3-test2" hash="case3-location2">
     External location 2
   </${tagContent}>
+
+  <${tagContent} id="case3-test3-1" hash="case3-nested1">
+    nested 1
+    <${tagContent} id="case3-test3-11" hash="case3-nested11">
+      nested 11
+      <${tagContent} id="case3-test3-111" hash="case3-nested111">
+        nested 111
+      </${tagContent}>
+    </${tagContent}>
+  </${tagContent}>
   `;
 
   var async1 = async_test('Case 3: hash changed to content[hash="case3-location1/case3-location2"]');
   var async2 = async_test('Case 3: hash changed to content[hash="case3-location1"]');
   var async3 = async_test('Case 3: hash changed to content[hash="case3-location2"]');
+  //var async4 = async_test('Case 3: hash changed to content[hash="case3-nested1/case3-nested11/case3-nested111"]');
 
   async1.next = async1.step_func(_ => {
     var check_hash = async1.step_func((e) => {
       window.removeEventListener('hashchange', check_hash);
 
-      var content1 = document.querySelector(`${tagContent}[hash="case3-location1"]`);
+      var content1 = document.querySelector(`${tagContent}#case3-test1-p`);
       var content2 = document.querySelector(`${tagContent}#case3-test1`);
       assert_false(content1.hidden);
       assert_false(content2.hidden);
@@ -43,7 +54,7 @@
       window.removeEventListener('hashchange', check_hash);
 
       //window.location.hash = '';// still having problems with this tear-down
-      var content1 = document.querySelector(`${tagContent}[hash="case3-location1"]`);
+      var content1 = document.querySelector(`${tagContent}#case3-test1-p`);
       var content2 = document.querySelector(`${tagContent}#case3-test1`);
       assert_false(content1.hidden);
       assert_true(content2.hidden);
@@ -60,7 +71,7 @@
       window.removeEventListener('hashchange', check_hash);
 
       //window.location.hash = '';// still having problems with this tear-down
-      var content1 = document.querySelector(`${tagContent}[hash="case3-location1"]`);
+      var content1 = document.querySelector(`${tagContent}#case3-test1-p`);
       var content2 = document.querySelector(`${tagContent}#case3-test1`);
       var content3 = document.querySelector(`${tagContent}#case3-test2`);
       assert_true(content1.hidden);
