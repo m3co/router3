53742dff9dd3599a932595044c4e61f58a5955f3
Andres Rios <rianby64@gmail.com>
2016-07-27 20:29:04 +0300

    Finish testing for case 6

--- test/case6/case6.js
+++ test/case6/case6.js
@@ -13,6 +13,9 @@
       Nested param
       <${tagContent} id="case6-111" hash="case(\\w+)-(\\d+)">
         Very interesting test
+        <${tagContent} id="case6-1111" hash="case(\\w+)">
+          Really interesting
+        </${tagContent}>
       </${tagContent}>
     </${tagContent}>
   </${tagContent}>
@@ -22,7 +25,7 @@
   var async2 = async_test('Case 6; hash changed to content[hash="case62"]');
   var async3 = async_test('Case 6: hash changed to content[hash="case(\\d+)/case(\\d+)"]');
   var async4 = async_test('Case 6; hash changed to content[hash="case62"] in order to reset last state');
-  //var async5 = async_test('Case 6: hash changed to content[hash="case(\\d+)/case(\\d+)/case(\\w+)-(\\d+)"]');
+  var async5 = async_test('Case 6: hash changed to content[hash="case(\\d+)/case(\\d+)/case(\\w+)-(\\d+)/case(\\w+)"]');
 
   async1.next = async1.step_func(_ => {
     var check_hash = async1.step_func((e) => {
@@ -84,15 +87,49 @@
       assert_equals(content2.getAttribute('route-param1'), null);
       assert_equals(content2.getAttribute('route-param2'), null);
 
-      document.body.removeChild(div);
       async4.done();
-      rc.next();
-
+      async5.next();
     });
     window.addEventListener('hashchange', check_hash);
     window.location.hash = "case62";
   });
 
+  async5.next = async5.step_func(_ => {
+    var check_hash = async5.step_func((e) => {
+      window.removeEventListener('hashchange', check_hash);
+      var content1 = document.querySelector('#case6-1');
+      var content2 = document.querySelector('#case6-11');
+      var content3 = document.querySelector('#case6-111');
+      var content4 = document.querySelector('#case6-1111');
+      assert_false(content1.hidden);
+      assert_false(content2.hidden);
+      assert_false(content3.hidden);
+      assert_false(content4.hidden);
+
+      assert_equals(content1.getAttribute('route-param1'), '555');
+
+      assert_equals(content2.getAttribute('route-param1'), '555');
+      assert_equals(content2.getAttribute('route-param2'), '666');
+
+      assert_equals(content3.getAttribute('route-param1'), '555');
+      assert_equals(content3.getAttribute('route-param2'), '666');
+      assert_equals(content3.getAttribute('route-param3'), 'AAA');
+      assert_equals(content3.getAttribute('route-param4'), '888');
+
+      assert_equals(content4.getAttribute('route-param1'), '555');
+      assert_equals(content4.getAttribute('route-param2'), '666');
+      assert_equals(content4.getAttribute('route-param3'), 'AAA');
+      assert_equals(content4.getAttribute('route-param4'), '888');
+      assert_equals(content4.getAttribute('route-param5'), 'BBB');
+
+      document.body.removeChild(div);
+      async5.done();
+      rc.next();
+
+    });
+    window.addEventListener('hashchange', check_hash);
+    window.location.hash = "case555/case666/caseAAA-888/caseBBB";
+  });
 
   rc.push(_ => {
     async1.step(_ => {
