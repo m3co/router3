b32b6eb1c875d6a3e1a6f11e8794e7d5790d91bf
Andres Rios <rianby64@gmail.com>
2016-07-28 09:25:39 +0300

    Add a second failing test to case 7

--- test/case7/case7.js
+++ test/case7/case7.js
@@ -16,11 +16,10 @@
   `;
 
   var async1 = async_test('Case 7: hash changed to content[hash="case(\\d+)"]');
-  //var async2 = async_test('Case 7: hash changed to content[hash="case(\\d+)/case(\\d+)"]');
+  var async2 = async_test('Case 7: hash changed to content[hash="case(\\d+)/case(\\d+)"]');
   //var async3 = async_test('Case 7; hash changed to content[hash="case(\\d+)/case(\\d+)/case(\\w+)-(\\d+)"] in order to reset last state');
 
   async1.next = async1.step_func(_ => {
-    window.location.hash = '';
     var content1 = document.querySelector('#case7-1');
     var order = [];
     var param1 = '123';
@@ -45,14 +44,58 @@
       assert_equals(e.detail.router, content1);
       assert_array_equals(order, ['show', 'hide']);
 
-      document.body.removeChild(div);
       async1.done();
-      rc.next();
+      async2.next();
     });
 
     content1.addEventListener('show', check_show);
     content1.addEventListener('hide', check_hide);
-    window.location.hash = "case" + param1;
+    window.location.hash = `case${param1}`;
+  });
+
+  async2.next = async2.step_func(_ => {
+    var content1 = document.querySelector('#case7-1');
+    var content2 = document.querySelector('#case7-11');
+    var order = [];
+    var param1 = '678';
+    var param2 = '432';
+
+    var check_show = async2.step_func((e) => {
+      order.push(e.type);
+
+      content2.removeEventListener(e.type, check_show);
+      assert_false(content1.hidden);
+      assert_false(content2.hidden);
+
+      assert_equals(e.detail.param1, param1);
+      assert_equals(e.detail.param2, param2);
+
+      //assert_equals(e.detail.router, content2);
+
+      //window.location.hash = '';
+    });
+
+    var check_hide = async2.step_func((e) => {
+      order.push(e.type);
+
+      content2.removeEventListener(e.type, check_hide);
+      assert_true(content1.hidden);
+      assert_true(content2.hidden);
+
+      assert_equals(e.detail.param1, param1);
+      assert_equals(e.detail.param2, param2);
+
+      //assert_equals(e.detail.router, content2);
+      assert_array_equals(order, ['show', 'hide']);
+
+      document.body.removeChild(div);
+      async2.done();
+      rc.next();
+    });
+
+    content2.addEventListener('show', check_show);
+    content2.addEventListener('hide', check_hide);
+    window.location.hash = `case${param1}/case${param2}`;
   });
 
   rc.push(_ => {
