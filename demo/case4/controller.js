fd5be4caad93959562a592b5043337458ff6546f
Andres Rios <rianby64@gmail.com>
2016-07-30 14:09:05 +0300

    Add a demo for nested show/hide events

---
+++ demo/case4/controller.js
@@ -0,0 +1,28 @@
+;(_ => {
+  'use strict';
+  var content1   = document.querySelector('#r1');
+  var content11  = document.querySelector('#r11');
+  var content111 = document.querySelector('#r111');
+
+  content1.addEventListener('show', e => {
+    console.log('content 1 show');
+  });
+  content1.addEventListener('hide', e => {
+    console.log('content 1 hide');
+  });
+
+  content11.addEventListener('show', e => {
+    console.log('content 11 show');
+  });
+  content11.addEventListener('hide', e => {
+    console.log('content 11 hide');
+  });
+
+  content111.addEventListener('show', e => {
+    console.log('content 111 show');
+  });
+  content111.addEventListener('hide', e => {
+    console.log('content 111 hide');
+  });
+
+})();
