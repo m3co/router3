734245fcf66c3aa528ba6b6f4f1a3c544c0eae10
Andres Rios <rianby64@gmail.com>
2016-07-30 15:20:36 +0300

    Change arrow functions to normal functions in demos

--- demo/case4/controller.js
+++ demo/case4/controller.js
@@ -4,24 +4,24 @@
   var content11  = document.querySelector('#r11');
   var content111 = document.querySelector('#r111');
 
-  content1.addEventListener('show', e => {
+  content1.addEventListener('show', function(e) {
     console.log('content 1 show');
   });
-  content1.addEventListener('hide', e => {
+  content1.addEventListener('hide', function(e) {
     console.log('content 1 hide');
   });
 
-  content11.addEventListener('show', e => {
+  content11.addEventListener('show', function(e) {
     console.log('content 11 show');
   });
-  content11.addEventListener('hide', e => {
+  content11.addEventListener('hide', function(e) {
     console.log('content 11 hide');
   });
 
-  content111.addEventListener('show', e => {
+  content111.addEventListener('show', function(e) {
     console.log('content 111 show');
   });
-  content111.addEventListener('hide', e => {
+  content111.addEventListener('hide', function(e) {
     console.log('content 111 hide');
   });
 
