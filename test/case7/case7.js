d247b3219f47f2888e3bc24515fdaa7c6e947759
Andres Rios <rianby64@gmail.com>
2016-07-30 13:00:27 +0300

    Add the last (integration) test for show/hide events

--- test/case7/case7.js
+++ test/case7/case7.js
@@ -22,6 +22,7 @@
   var async5 = async_test('Case 7: (show event) hash changed to content[hash="case(\\d+)/case(\\d+)/case(\\w+)-(\\d+)"]');
   var async6 = async_test('Case 7: (hide event) hash changed to content[hash="case(\\d+)/case(\\d+)"]');
   var async7 = async_test('Case 7: (hide event) hash changed to content[hash="case(\\d+)/case(\\d+)/case(\\w+)-(\\d+)"]');
+  var async8 = async_test('Case 7: (show/hide event) hash changed to content[hash="case(\\d+)/case(\\d+)/case(\\w+)-(\\d+)"]');
 
   async1.next = async1.step_func(_ => {
     var content1 = document.querySelector('#case7-1');
@@ -258,14 +259,94 @@
       window.location.hash = '';
     }), 0);
     setTimeout(async7.step_func(_ => {
-      document.body.removeChild(div);
-      rc.next(); // do you see this line? should I repeat this solution
+      async8.next(); // do you see this line? should I repeat this solution
                  // along to all other tests? I should...
       assert_array_equals(order, ['1', '2', '3']);
       async7.done();
     }), 0);
   });
 
+  async8.next = async8.step_func(_ => {
+    var order = [];
+    var content1 = document.querySelector('#case7-1');
+    var content2 = document.querySelector('#case7-11');
+    var content3 = document.querySelector('#case7-111');
+    var param1 = '123';
+    var param2 = '654';
+    var param3 = 'abc';
+    var param4 = '789';
+
+    var check_show1 = async8.step_func((e) => {
+      order.push('3s');
+      content1.removeEventListener(e.type, check_show1);
+      assert_false(content1.hidden);
+      assert_equals(e.detail.param1, param1);
+    });
+
+    var check_show2 = async8.step_func((e) => {
+      order.push('2s');
+      content2.removeEventListener(e.type, check_show2);
+      assert_false(content2.hidden);
+      assert_equals(e.detail.param1, param1);
+      assert_equals(e.detail.param2, param2);
+    });
+
+    var check_show3 = async8.step_func((e) => {
+      order.push('1s');
+      content3.removeEventListener(e.type, check_show3);
+      assert_false(content3.hidden);
+      assert_equals(e.detail.param1, param1);
+      assert_equals(e.detail.param2, param2);
+      assert_equals(e.detail.param3, param3);
+      assert_equals(e.detail.param4, param4);
+    });
+
+    var check_hide1 = async8.step_func((e) => {
+      order.push('3h');
+      content1.removeEventListener(e.type, check_hide1);
+      assert_false(content1.hidden);  /// WHY??
+      assert_equals(e.detail.param1, param1);
+    });
+
+    var check_hide2 = async8.step_func((e) => {
+      order.push('2h');
+      content2.removeEventListener(e.type, check_hide2);
+      assert_false(content2.hidden);  /// WHY??
+      assert_equals(e.detail.param1, param1);
+      assert_equals(e.detail.param2, param2);
+    });
+
+    var check_hide3 = async8.step_func((e) => {
+      order.push('1h');
+      content3.removeEventListener(e.type, check_hide3);
+      assert_false(content3.hidden);  /// WHY??
+      assert_equals(e.detail.param1, param1);
+      assert_equals(e.detail.param2, param2);
+      assert_equals(e.detail.param3, param3);
+      assert_equals(e.detail.param4, param4);
+    });
+
+    content1.addEventListener('show', check_show1);
+    content2.addEventListener('show', check_show2);
+    content3.addEventListener('show', check_show3);
+
+    content1.addEventListener('show', check_hide1);
+    content2.addEventListener('show', check_hide2);
+    content3.addEventListener('show', check_hide3);
+    window.location.hash = `case${param1}/case${param2}/case${param3}-${param4}`;
+
+    setTimeout(async8.step_func(_ => {
+      window.location.hash = '';
+    }), 0);
+    setTimeout(async8.step_func(_ => {
+      document.body.removeChild(div);
+      rc.next(); // do you see this line? should I repeat this solution
+                 // along to all other tests? I should...
+      // RECONSIDER THE ORDER OF EXECUTION
+      assert_array_equals(order, ['1s', '1h', '2s', '2h', '3s', '3h']);
+      async8.done();
+    }), 0);
+  });
 
   rc.push(_ => {
     async1.step(_ => {
