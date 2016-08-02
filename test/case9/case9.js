9ed639bb6236c89513e95078ec740d1ef53a9789
Andres Rios <rianby64@gmail.com>
2016-08-02 22:11:08 +0300

    Add first failing test for case 9

--- test/case9/case9.js
+++ test/case9/case9.js
@@ -33,13 +33,40 @@
       Case 9-2
     </${tagContent}>
 
-    <${tagContent} hash="case9-3">
+    <${tagContent} hash="case9-4">
       Case 9-2
     </${tagContent}>
 
-    <${tagContent} hash="case9-3" class-show="" class-hide="">
+    <${tagContent} hash="case9-5" class-show="" class-hide="">
       Case 9-2
     </${tagContent}>
   `;
+
+  var async1 = async_test('Case 9: hash changed to content[hash="case9-1"]');
+
+  async1.next = async1.step_func(_ => {
+    var content1 = document.querySelector(`${tagContent}[hash="case9-1"]`);
+    var content2 = document.querySelector(`${tagContent}[hash="case9-2"]`);
+    var content3 = document.querySelector(`${tagContent}[hash="case9-3"]`);
+    var content4 = document.querySelector(`${tagContent}[hash="case9-4"]`);
+    var content5 = document.querySelector(`${tagContent}[hash="case9-5"]`);
+
+    assert_equals(content1.getAttribute(specialHideAttr), '');
+    assert_equals(content2.getAttribute(specialHideAttr), '');
+    assert_equals(content3.getAttribute(specialHideAttr), '');
+    assert_equals(content4.getAttribute(specialHideAttr), '');
+    assert_equals(content5.getAttribute(specialHideAttr), '');
+
+    rc.next();
+    document.body.removeChild(div);
+    async1.done();
+  });
+
+  rc.push(_ => {
+    async1.step(_ => {
+      document.body.appendChild(div);
+      async1.next();
+    });
+  });
 
 })(window.routeCases);
