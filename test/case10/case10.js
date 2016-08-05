31f487901b141385fe68425bc6f83dc415fd2911
Andres Rios <rianby64@gmail.com>
2016-08-05 23:47:48 +0300

    Add last test for the last nested view

--- test/case10/case10.js
+++ test/case10/case10.js
@@ -38,7 +38,7 @@
 
   var async1 = async_test('Case 10: hash changed to content[hash="case10-1"]');
   var async2 = async_test('Case 10: hash changed to content[hash="case10-1/case10-11"]');
-  //var async3 = async_test('Case 10: hash changed to content[hash="case10-1/case10-11/case10-111"]');
+  var async3 = async_test('Case 10: hash changed to content[hash="case10-1/case10-11/case10-111"]');
 
   async1.next = async1.step_func(_ => {
     var hash = "case10-1";
@@ -96,6 +96,38 @@
         assert_true(content4.hidden);
         assert_true(content5.hidden);
         async2.done();
+        async3.next();
+      }, 10);
+      content1.removeEventListener('hide', check_hash_def);
+    });
+
+    content1.addEventListener('show', check_hash_1);
+    window.location.hash = "case10-1/" + hash;
+  });
+
+  async3.next = async3.step_func(_ => {
+    var hash = "case10-111";
+    var content1 = document.querySelector(`${tagContent}[hash="${hash}"]`);
+    var content2 = document.querySelector(`${tagContent}#def`);
+    var content3 = document.querySelector(`${tagContent}#defC`);
+    var content4 = document.querySelector(`${tagContent}#defB`);
+    var content5 = document.querySelector(`${tagContent}#defA`);
+
+    var check_hash_1 = async3.step_func((e) => {
+      assert_false(content1.hidden);
+      assert_true(content2.hidden);
+      assert_true(content3.hidden);
+      assert_true(content4.hidden);
+      assert_true(content5.hidden);
+
+      content1.removeEventListener('show', check_hash_1);
+      content1.addEventListener('hide', check_hash_def);
+      window.location.hash = '';
+    });
+
+    var check_hash_def = async3.step_func(e => {
+      setTimeout(_ => {
+        async3.done();
 
         document.body.removeChild(div);
         rc.next();
@@ -104,8 +136,9 @@
     });
 
     content1.addEventListener('show', check_hash_1);
-    window.location.hash = "case10-1/" + hash;
+    window.location.hash = "case10-1/case10-11/" + hash;
   });
+
 
   rc.push(_ => {
     async1.step(_ => {
