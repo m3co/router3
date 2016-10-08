3a543971deb33bdf11fd810d2050bc2b12e66068
Andres Rios <rianby64@gmail.com>
2016-10-08 03:05:01 +0300

    Close #9

--- test/case12/case12.js
+++ test/case12/case12.js
@@ -74,31 +74,45 @@
 
     var hash1 = "case12-12";
     var hash2 = "case12-11";
-    var defaultHash = "";
+    var defaultHash = "case12-1";
     var content1 = document.querySelector(`${tagContent}[hash="${hash1}"]`);
     var content2 = document.querySelector(`${tagContent}[hash="${hash2}"]`);
-    var defaultContent = document.querySelector(`${tagContent}#case12-1-default`);
+    var defaultContent2 = document.querySelector(`${tagContent}#case12-1-default`);
+    var defaultContent1 = document.querySelector(`${tagContent}#case12-default`);
 
-    var check_default = async2.step_func(e => {
-      assert_false(defaultContent.hidden);
+    var check_default2 = async2.step_func(e => {
+      assert_true(defaultContent1.hidden);
+      assert_false(defaultContent2.hidden);
 
       window.location.hash = 'case12-1/' + hash2;
     });
 
-    var check_default_hide = async2.step_func(e => {
-      assert_true(defaultContent.hidden);
+    var check_default_hide2 = async2.step_func(e => {
+      assert_true(defaultContent1.hidden);
+      assert_true(defaultContent2.hidden);
 
+      async2.done();
       // clean the test
       document.body.removeChild(div);
       window.location.hash = '';
-      async2.done();
+    });
+
+    var check_default1 = async2.step_func(e => {
+      assert_unreached();
+    });
+
+    var check_default_hide1 = async2.step_func(e => {
+      assert_unreached();
     });
 
     var check_hash = async2.step_func((e) => {
       content1.removeEventListener('show', check_hash);
 
-      defaultContent.addEventListener('show', check_default);
-      defaultContent.addEventListener('hide', check_default_hide);
+      defaultContent1.addEventListener('show', check_default1);
+      defaultContent1.addEventListener('hide', check_default_hide1);
+
+      defaultContent2.addEventListener('show', check_default2);
+      defaultContent2.addEventListener('hide', check_default_hide2);
 
       window.location.hash = defaultHash;
     });
