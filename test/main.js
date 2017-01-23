d4f5d2aa34a02101fe897d4f874af2f638d7f116
Andres Rios <rianby64@gmail.com>
2017-01-23 17:56:15 +0300

    [TEST] Push this current state to keep on working on while I'm installing something under Windows...

--- test/main.js
+++ test/main.js
@@ -1,4 +1,4 @@
-(() => {
+window.addEventListener('load', () => {
 
   const classAsString = "MaterialRouter3";
   const cssRouter = "mdl-router3";
@@ -11,7 +11,7 @@
     return document.querySelector(`[hash="${el}"]`);
   }
 
-  onload_test(function() {
+  async_test(function() {
     var router3 = document.querySelector(selRouter);
 
     assert_true(router3.MaterialRouter3 instanceof MaterialRouter3);
@@ -20,7 +20,7 @@
   }, "Check API");
 
 
-  onload_test(function() {
+  async_test(function() {
     // [setup]
     let hash1 = selectHash("hash1");
     let hash2 = selectHash("hash2");
@@ -32,13 +32,39 @@
 
       // [teardown]
       this.done();
+      console.log(2);
     });
     window.addEventListener('hashchange', handler);
     assert_true(hash1.hidden);
     assert_true(hash2.hidden);
+    console.log(1);
 
     // [run]
     window.location.hash = "hash1";
   }, "Change route from '' to '#hash1'");
 
-})();
+
+  async_test(function() {
+    // [setup]
+    let hash1 = selectHash("hash1");
+    let hash2 = selectHash("hash2");
+
+    let handler = this.step_func((e) => {
+      // [verify]
+      assert_true(hash1.hidden);
+      assert_false(hash2.hidden);
+
+      // [teardown]
+      this.done();
+      console.log(4);
+    });
+    window.addEventListener('hashchange', handler);
+    assert_true(hash1.hidden);
+    assert_true(hash2.hidden);
+    console.log(3);
+
+    // [run]
+    window.location.hash = "hash2";
+  }, "Change route from '' to '#hash2'");
+
+});
