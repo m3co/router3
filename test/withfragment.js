6805f4efcf2b1ca3aa0b367346d4daae252852e5
Andres Rios <rianby64@gmail.com>
2017-01-29 02:39:38 +0300

    [TEST] Show that router3 integrates with fragment

--- test/withfragment.js
+++ test/withfragment.js
@@ -12,21 +12,22 @@ window.addEventListener('load', () => {
     let handler1 = this.step_func((e) => {
       hash2 = selectHash("fragment");
       hash2.addEventListener('show', handler2);
+
+      // [run]
+      window.location.hash = "main/fragment";
     });
     hash1.addEventListener('load', handler1);
 
     let handler2 = this.step_func((e) => {
       // [verify]
-      assert_true(hash2.hidden);
-      assert_true(hash1.hidden);
+      assert_false(hash2.hidden);
+      assert_false(hash1.hidden);
 
       // [teardown]
       hash2.removeEventListener('load', handler1);
       teardown(resolve, handler2, e);
     });
 
-    // [run]
-    window.location.hash = "main/fragment";
   }); }, "Integrate");
 
 });
