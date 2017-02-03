4184fea93ca3d58486018b8213ecdb39952a1d53
Daniyal Gabitov <danuy@list.ru>
2017-02-03 15:30:08 +0300

    Improve gulpfile code

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,22 @@
 'use strict';
-const gulp = require('gulp');
-const csslint = require('gulp-csslint');
+
+const gulp     = require('gulp');
+const csslint  = require('gulp-csslint');
 const htmlhint = require("gulp-htmlhint");
-const eslint = require('gulp-eslint');
-const babel = require('gulp-babel');
-const connect = require('gulp-connect');
-const jsdoc = require('gulp-jsdoc3');
-const gulpIf = require('gulp-if');
+const eslint   = require('gulp-eslint');
+const babel    = require('gulp-babel');
+const connect  = require('gulp-connect');
+const jsdoc    = require('gulp-jsdoc3');
+const gulpIf   = require('gulp-if');
 
 const paths = {
-  src: 'src', dst: './',
-  css:   ['demo/*.css', 'demo/**/*.css',
-          'test/*.css', 'test/**/*.css',
-          'src/*.css',   'src/**/*.css'],
-  html:  ['demo/*.html', 'demo/**/*.html',
-          'test/*.html', 'test/**/*.html'],
-  js:    ['demo/*.js',   'demo/**/*.js',
-          'test/*.js',   'test/**/*.js',
-          'src/*.js',    'src/**/*.js'],
-  csssrc:['src/*.css',   'src/**/*.css'],
-  jssrc: ['./src/*.js',    './src/**/*.js']
+  src:    'src',
+  dst:    './',
+  css:    ['demo/**/*.css', 'test/**/*.css', 'src/**/*.css'],
+  html:   ['demo/**/*.html', 'test/**/*.html'],
+  js:     ['demo/**/*.js', 'test/**/*.js', 'src/**/*.js'],
+  csssrc: ['src/**/*.css'],
+  jssrc:  ['./src/**/*.js']
 };
 
 function isFixed(file) {
@@ -32,50 +29,51 @@ gulp.task('reload', function () {
     .pipe(connect.reload());
 });
 
-gulp.task('doc', function () {
+gulp.task('doc', () => {
   var config = require('./.jsdoc.json');
-  return gulp.src(['./README.md'].concat(paths.jssrc), { read: false })
+  return gulp.src(['./README.md']
+    .concat(paths.jssrc), { read: false })
     .pipe(jsdoc(config));
 });
 
-gulp.task('css-lint', function() {
+gulp.task('css-lint', () => {
   return gulp.src(paths.css)
     .pipe(csslint())
     .pipe(csslint.formatter());
 });
 
-gulp.task('html-hint', _ => {
+gulp.task('html-hint', () => {
   return gulp.src(paths.html)
     .pipe(htmlhint())
     .pipe(htmlhint.reporter());
 });
 
-gulp.task('js-lint', _ => {
-  return gulp.src(paths.jssrc, {base: './'})
+gulp.task('js-lint', () => {
+  return gulp.src(paths.jssrc, { base: './' })
     .pipe(eslint({fix: true}))
     .pipe(eslint.format())
     .pipe(gulpIf(isFixed, gulp.dest('./')));
 });
 
-gulp.task('js-copy', _ => {
+gulp.task('js-copy', () => {
   return gulp.src(paths.jssrc)
     .pipe(babel())
     .pipe(gulp.dest(paths.dst));
 });
 
-gulp.task('css-copy', _ => {
+gulp.task('css-copy', () => {
   return gulp.src(paths.csssrc)
     .pipe(gulp.dest(paths.dst));
 });
 
-gulp.task('watch', _ => {
+gulp.task('watch', () => {
   gulp.watch(paths.js, ['reload']);
   gulp.watch(paths.html, ['html-hint', 'doc', 'reload']);
   gulp.watch(paths.jssrc, ['js-lint', 'js-copy', 'doc', 'reload']);
   gulp.watch(paths.csssrc, ['css-lint', 'css-copy', 'reload']);
 });
 
-gulp.task('connect', function() {
+gulp.task('connect', () => {
   connect.server({
     root: paths.dst,
     livereload: true,
@@ -93,4 +91,3 @@ gulp.task('default', [
   'watch',
   'connect'
 ]);
-
