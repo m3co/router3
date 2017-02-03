0814ec2cca21a8c3f9e878b9e5397d0199e1d2de
Andres Rios <rianby64@gmail.com>
2017-02-03 20:56:20 +0300

    [TEST] Show event happens earlier than routing

--- test/withfragment.js
+++ test/withfragment.js
@@ -7,26 +7,44 @@ window.addEventListener('load', () => {
   promise_test(function() { return new Promise((resolve, reject) => {
     // [setup]
     let hash1 = selectHash("main");
-    let hash2;
+    let hash3 = selectHash("detached");
 
-    let handler1 = this.step_func((e) => {
-      hash2 = selectHash("fragment");
-      hash2.addEventListener('show', handler2);
-    });
-    hash1.addEventListener('load', handler1);
+    let handler1 = this.step_func((e) => { this.step_timeout(() => {
+      // [verify]
+      assert_false(hash1.hidden);
+      assert_true(hash3.hidden);
 
-    let handler2 = this.step_func((e) => {
+      // [teardown]
+      hash1.removeEventListener('show', handler1);
+      teardown(resolve, handler1, e)
+    }, 100); });
+    hash1.addEventListener('show', handler1);
+
+    // [run]
+    window.location.hash = "main";
+  }); }, "Go to main");
+
+  promise_test(function() { return new Promise((resolve, reject) => {
+    // [setup]
+    let hash1 = selectHash("main");
+    let hash2 = selectHash(hash1, "fragment");
+    let hash3 = selectHash("detached");
+    let hash4 = selectHash(hash3, "fragment");
+
+    let handler2 = this.step_func((e) => { this.step_timeout(() => {
       // [verify]
       assert_false(hash2.hidden);
       assert_false(hash1.hidden);
+      assert_true(hash3.hidden);
+      assert_true(hash4.hidden);
 
       // [teardown]
-      hash2.removeEventListener('load', handler1);
       teardown(resolve, handler2, e);
-    });
+    }, 100); });
+    hash2.addEventListener('show', handler2);
 
     // [run]
     window.location.hash = "main/fragment";
-  }); }, "Integrate");
+  }); }, "Go to main/fragment");
 
 });
