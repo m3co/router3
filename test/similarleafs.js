39a1c767750eb81accd99c17ae480bb4f3b07fbf
Andres Rios <rianby64@gmail.com>
2017-02-20 14:20:10 +0300

    [TEST] Navigation to fragments with same name
    Related to #53

---
+++ test/similarleafs.js
@@ -0,0 +1,137 @@
+window.addEventListener('load', () => {
+
+  const classAsString = "MaterialRouter3";
+  const cssRouter = "mdl-router3";
+  const selRouter = `.${cssRouter}`;
+
+  promise_test(function() { return new Promise((resolve, reject) => {
+    // [setup]
+    let hash1 = selectHash("main");
+    let hash3 = selectHash("detached");
+
+    let handler1 = this.step_func((e) => { this.step_timeout(() => {
+      // [verify]
+      assert_true(hash1.hidden);
+      assert_false(hash3.hidden);
+
+      // [teardown]
+      hash3.removeEventListener('show', handler1);
+      teardown(resolve, handler1, e)
+    }, 100); });
+    hash3.addEventListener('show', handler1);
+
+    assert_true(!!hash1.MaterialRouter3);
+    assert_true(!!hash3.MaterialRouter3);
+
+    // [run]
+    window.location.hash = "detached";
+  }); }, "Go to detached");
+
+  promise_test(function() { return new Promise((resolve, reject) => {
+    // [setup]
+    let hash1 = selectHash("main");
+    let hash3 = selectHash("detached");
+
+    let handler1 = this.step_func((e) => { this.step_timeout(() => {
+      // [verify]
+      assert_false(hash1.hidden);
+      assert_true(hash3.hidden);
+
+      // [teardown]
+      hash1.removeEventListener('show', handler1);
+      teardown(resolve, handler1, e)
+    }, 100); });
+    hash1.addEventListener('show', handler1);
+
+    assert_true(!!hash1.MaterialRouter3);
+    assert_true(!!hash3.MaterialRouter3);
+
+    // [run]
+    window.location.hash = "main";
+  }); }, "Go to main");
+
+  promise_test(function() { return new Promise((resolve, reject) => {
+    // [setup]
+    let hash1 = selectHash("main");
+    let hash2 = selectHash(hash1, "fragment");
+    let hash3 = selectHash("detached");
+    let hash4 = selectHash(hash3, "fragment");
+
+    let handler2 = this.step_func((e) => { this.step_timeout(() => {
+      // [verify]
+      assert_false(hash2.hidden);
+      assert_false(hash1.hidden);
+      assert_true(hash3.hidden);
+      assert_true(hash4.hidden);
+
+      // [teardown]
+      teardown(resolve, handler2, e);
+    }, 100); });
+    hash2.addEventListener('show', handler2);
+
+    assert_true(!!hash1.MaterialRouter3);
+    assert_true(!!hash2.MaterialRouter3);
+    assert_true(!!hash3.MaterialRouter3);
+    assert_true(!!hash4.MaterialRouter3);
+
+    // [run]
+    window.location.hash = "main/fragment";
+  }); }, "Go to main/fragment take 1");
+
+  promise_test(function() { return new Promise((resolve, reject) => {
+    // [setup]
+    let hash1 = selectHash("main");
+    let hash2 = selectHash(hash1, "fragment");
+    let hash3 = selectHash("detached");
+    let hash4 = selectHash(hash3, "fragment");
+
+    let handler2 = this.step_func((e) => { this.step_timeout(() => {
+      // [verify]
+      assert_true(hash1.hidden);
+      assert_true(hash2.hidden);
+      assert_false(hash3.hidden);
+      assert_false(hash4.hidden);
+
+      // [teardown]
+      teardown(resolve, handler2, e);
+    }, 100); });
+    hash4.addEventListener('show', handler2);
+
+    assert_true(!!hash1.MaterialRouter3);
+    assert_true(!!hash2.MaterialRouter3);
+    assert_true(!!hash3.MaterialRouter3);
+    assert_true(!!hash4.MaterialRouter3);
+
+    // [run]
+    window.location.hash = "detached/fragment";
+  }); }, "Go to detached/fragment");
+
+  promise_test(function() { return new Promise((resolve, reject) => {
+    // [setup]
+    let hash1 = selectHash("main");
+    let hash2 = selectHash(hash1, "fragment");
+    let hash3 = selectHash("detached");
+    let hash4 = selectHash(hash3, "fragment");
+
+    let handler2 = this.step_func((e) => { this.step_timeout(() => {
+      // [verify]
+      assert_false(hash2.hidden);
+      assert_false(hash1.hidden);
+      assert_true(hash3.hidden);
+      assert_true(hash4.hidden);
+
+      // [teardown]
+      teardown(resolve, handler2, e);
+    }, 100); });
+    hash2.addEventListener('show', handler2);
+
+    assert_true(!!hash1.MaterialRouter3);
+    assert_true(!!hash2.MaterialRouter3);
+    assert_true(!!hash3.MaterialRouter3);
+    assert_true(!!hash4.MaterialRouter3);
+
+    // [run]
+    window.location.hash = "main/fragment";
+  }); }, "Go to main/fragment take 2");
+
+});
